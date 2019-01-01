02e62bf10632b3373fa58144636051ddd94789f8
Adam Ivansky <adam.ivansky@gmail.com>
2019-01-01 14:19:33 +0100

    strugging with gloval vars issue

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ import json
 import math
 import random
 
-import sched, time
+from threading import Timer
+from time import sleep
+
+import signal
+import sys
 
 def my_init():
     
@@ -37,7 +41,7 @@ def my_update(controls, player_id, map_in, context):
     
     objects = []
     
-    for obj_id,obj_out in enumerate(objects_out):
+    for obj_id, obj_out in enumerate(objects_out):
         objects.append({})
         
         objects[len(objects)-1]['id'] = obj_out.object_id
@@ -47,11 +51,9 @@ def my_update(controls, player_id, map_in, context):
         objects[len(objects)-1]['top'] = obj_out.world_x - player_object.world_x - obj_out.sz_x/2 + 200
         objects[len(objects)-1]['left'] = obj_out.world_y - player_object.world_y - obj_out.sz_y/2 + 200
         objects[len(objects)-1]['backgroundColor'] = ""
-        objects[len(objects)-1]['backgroundImage'] = 'url("get_image/'+obj_out.get_image()+'_orig_'+str(0)+'.png")'
+        objects[len(objects)-1]['backgroundImage'] = obj_out.get_image()
         objects[len(objects)-1]['zIndex'] = obj_out.z_index
-
-    objects.append({})
-    
+        
     context["time"] = context["time"] + 1
     
     angle = 180+10*int(math.atan2(float(controls["mouse_x"])-200, float(controls["mouse_y"])-200)/(2*math.pi)*360/10)
@@ -60,16 +62,6 @@ def my_update(controls, player_id, map_in, context):
     player_control_vy = - math.cos(angle/360*2*math.pi)*5
     
     map_in.control_player(player_id, player_control_vx, player_control_vy, angle)
-    
-    objects[len(objects)-1]['id'] = "player0"
-    objects[len(objects)-1]['width'] = 50 
-    objects[len(objects)-1]['height'] = 50 
-    objects[len(objects)-1]['position'] = "absolute" 
-    objects[len(objects)-1]['top'] = 175
-    objects[len(objects)-1]['left'] = 175
-    objects[len(objects)-1]['backgroundColor'] = ""
-    objects[len(objects)-1]['backgroundImage'] = 'url("get_image/plane_orig_'+str(angle)+'.png")'
-    objects[len(objects)-1]['zIndex'] = 1000
     
     return objects, context
 
@@ -93,8 +85,18 @@ class Map_object:
         return(self.state)
     
     def update_pos(self, dt):
-        self.world_x = self.world_x + self.world_vx*dt
-        self.world_y = self.world_y + self.world_vy*dt
+        self.world_x = self.world_x + int(self.world_vx)*dt
+        self.world_y = self.world_y + int(self.world_vy)*dt
+        if (self.object_type=='player') :
+            print(self.world_vx*dt,self.world_vy*dt )
+    
+    def __str__(self):
+        my_str = (
+            self.object_id + ' [' + self.object_type + ']'
+        )
+        return(my_str)
+        
+    __repr__ = __str__
 
 class Sphere_blue(Map_object):
     
@@ -103,7 +105,7 @@ class Sphere_blue(Map_object):
         Map_object.__init__(self, object_id, 'sphere_blue', world_x, world_y, 0, 0, 50, 50, 0, 10, 'idle')
         
     def get_image(self):
-        return('sphere_blue')      
+        return('url("get_image/sphere_blue_orig_'+str(int(self.rotation/10)*10)+'.png")')
 
     def update_state(self,dt):
         pass
@@ -117,7 +119,7 @@ class Orb(Map_object):
         self.timer = 0 
         
     def get_image(self):
-        return('orb')  
+        return('url("get_image/orb_orig_'+str(int(self.rotation/10)*10)+'.png")')  
     
     def update_state(self,dt):
         self.timer = self.timer + dt
@@ -128,7 +130,6 @@ class Orb(Map_object):
 class Player(Map_object):
     
     def __init__(self, object_id, world_x, world_y, world_vx, world_vy ):
-        
         Map_object.__init__(self, object_id, 'player', world_x, world_y, world_vx, world_vy, 50, 50, 0, 10, 'online')
         
     def get_image(self):
@@ -138,14 +139,14 @@ class World_map:
 
     def __init__(self, sz_x, sz_y):
         
-        self.dt = 1
+        self.dt = 5
         
         self.sz_x = sz_x
         self.sz_y = sz_y
         
         self.object_list = []
         
-        for i in range(0,3000):
+        for i in range(0,10):
             self.object_list.append(
                     Sphere_blue(
                             'sphere' + str(i), 
@@ -154,21 +155,35 @@ class World_map:
                             )
                     )
         
-        self.scheduller = sched.scheduler(time.time, time.sleep)
-        self.scheduller.run()
-        self.scheduller.enter(0.05, 1, self.update, argument=(), kwargs={})
+        self.thread_timer = Timer(0.1, self.update)
+        self.thread_timer.start()
+        self.active_timer = 1
         
     def iterate():
         pass
     
     def control_player(self, player_id, world_vx, world_vy, rotation): 
         
+        player_obj_id = 0
+        
+        for obj_id,my_obj in enumerate(self.object_list): 
+            if (my_obj.object_id==player_id):
+                player_obj_id = obj_id
+                
+        player_obj = self.object_list[player_obj_id]
+        
+        player_obj.rotation = rotation
+        player_obj.world_vx = world_vx
+        player_obj.world_vy = world_vy
+        
+        self.object_list[player_obj_id] = player_obj
+        
         return 0
 
     def get_object(self, object_id): 
         
         for my_obj in self.object_list: 
-            if (my_obj.id==object_id):
+            if (my_obj.object_id==object_id):
                 return(my_obj)
         
         return 0
@@ -177,18 +192,25 @@ class World_map:
         
         self.object_list.append(my_object)
         
+        print(self.object_list)
+        
         return 0
 
     def update(self):
-    
+        
+        print(self.object_list)
+        
         for my_obj in self.object_list:
             my_obj.update_pos(self.dt)
             my_obj.update_state(self.dt)
-
+        
         self.object_list = [my_obj for my_obj in self.object_list if not(my_obj.get_state()=='deleted')]  
         
-        self.scheduler.enter(0.05, 1, self.update, argument=(), kwargs={})
-        
+        if (self.active_timer):
+            self.thread_timer = Timer(0.1, self.update)
+            self.thread_timer.start()
+            
+            
     def get_objects(self, x_center, y_center, box_h, box_w):    
         
         obj_out = [el for el in self.object_list if (
@@ -199,11 +221,22 @@ class World_map:
             )]
         
         return(obj_out)
+
+    def close(self):
+        print('ending the class')
+        self.active_timer = 0
+
+    def __del__(self):
+        print('ending the class')        
+        self.active_timer = 0
         
+
 class my_environment:
 
     def __init__(self, update_function, init_function):
 
+        print('environment init')
+        
         self.objects = []
         self.context = []
         
@@ -217,6 +250,13 @@ class my_environment:
         self.update_function = update_function
         
         self.map = World_map(5000,5000)
+        
+    def __enter__(self):
+        return(self)
+    
+    def add_player(self, player_id):
+        
+        self.map.add_object( Player(player_id, 2500, 2500, 0, 0 ) )
         
     def update(self, player_id, controls):
         
@@ -265,8 +305,13 @@ class my_environment:
             instruction_list.append(instruction)
         
         return(instruction_list)
+
+    def __exit__(self ,type, value, traceback):
         
-global environment
+        self.map.close()
+        
+        return(False)
+
 
 app = Flask(__name__)
 app.debug = True
@@ -274,36 +319,45 @@ app.secret_key = "super secret key"
 
 @app.route("/update", methods=['GET', 'POST'])
 def update():
-    
+    global global_environment
     if request.method == "POST":
         control_data = request.get_json()
-        json_out = json.dumps( environment.update(session['user_id'], control_data) )
+        json_out = json.dumps( global_environment.update(session['user_id'], control_data) )
         return( json.dumps( json_out ) )
         
     return("ERROR")
     
 @app.route("/redraw", methods=['GET', 'POST'])
 def redraw():
-    
+    global global_environment
     if request.method == "POST":
-        json_out = json.dumps( environment.redraw(session['user_id']) )
+        json_out = json.dumps( global_environment.redraw(session['user_id']) )
         return( json.dumps( json_out ) )
     
     return("ERROR")
 
 @app.route('/login', methods=['GET', 'POST'])
 def do_login():
+    global global_environment
+    
+    session['stage'] = 2
     
     session['user_id'] = request.form['user_id']
+    
+    global_environment.add_player(session['user_id'])
     
     return index()
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if not session.get('user_id'):
+    if not session.get('stage'):
         return render_template('login.html')
     else:
-        return render_template("index.html")
+        if (not(session['stage']==2)):
+            return render_template('login.html')
+        else:
+            session['stage']=1
+            return render_template("index.html")
 
 @app.route("/get_image/<img_file>", methods=['GET', 'POST'])
 def get_img(img_file):
@@ -311,5 +365,10 @@ def get_img(img_file):
     return send_file("img/rotations/"+img_file, mimetype='image/png')
 
 if __name__ == "__main__":
-    environment = my_environment(my_update, my_init)
-    app.run()
+    
+    global global_environment
+    
+    with my_environment(my_update, my_init) as env1:
+        global_environment = env1
+        setattr(g, '_messages', messages)
+        app.run()
