2fd88a976103893a435be68725bc58385c9d91b9
Adam Ivansky <adam.ivansky@gmail.com>
2019-01-08 15:17:39 +0100

    init screen fixed

--- flask_API.py
+++ flask_API.py
@@ -15,6 +15,9 @@ import json, os
 from captcha.image import ImageCaptcha
 
 import redis
+
+import random
+
 obj_data_queue = redis.Redis(host='localhost', port=6379, db=0)
 con_data_queue = redis.Redis(host='localhost', port=6379, db=1)
 user_data_queue = redis.Redis(host='localhost', port=6379, db=2)
@@ -23,6 +26,14 @@ log_queue = redis.Redis(host='localhost', port=6379, db=3)
 app = Flask(__name__)
 app.debug = True
 app.secret_key = "super secret key"
+
+def generate_capcha():
+    
+    numbers = ['1','2','3','4','5','6','7','8','9','0']
+    alphabet_lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    random_list = [random.choice(alphabet_lowercase+numbers) for i in range(0,4) ]
+    str1 = ''.join(str(e) for e in random_list)
+    return(str1)
 
 def get_instr_update(objects_old, player_id):
     
@@ -33,7 +44,7 @@ def get_instr_update(objects_old, player_id):
         objects_new = []
     
     instruction_list = objects_new
-    
+     
     return(instruction_list, objects_new)
 
 def get_instr_redraw(player_id):
@@ -70,39 +81,47 @@ def update():
 @app.route('/login', methods=['GET', 'POST'])
 def do_login():
     
-    if ('1234'==request.form['capcha']):
-    
-        session['stage'] = 2
-        session['user_id'] = request.form['user_id']
+    if (session['capcha_solution']==request.form['capcha']):
         
-        session['objects'] = []
-        
-        user_data_queue.set('new_user',json.dumps( session['user_id'] ), px = 200)
-    
-        return index()
+        if (len(request.form['user_id'])<8):
+            return login_screen('short_user_id')
+        else:
+            player_id = request.form['user_id']
+            
+            objects_new_raw = obj_data_queue.get(player_id)
+            if (objects_new_raw is None):
+            
+                session['user_id'] = player_id    
+                session['objects'] = []
+                
+                user_data_queue.set('new_user',json.dumps( session['user_id'] ), px = 200)
+                
+                return game_renderer()
+            else:
+                return login_screen('player_exists')
     else:
-        return render_template('frame_set.html')
+        return login_screen('incorrect_capcha')
     
 @app.route('/get_capcha_img1', methods=['GET', 'POST'])
 def get_capcha_img():
     
     image = ImageCaptcha()
-    session['capcha_solution'] = 1234
-    data = image.generate(str(session['capcha_solution']))
+    session['capcha_solution'] = generate_capcha()
+    data = image.generate(session['capcha_solution'])
     
     return send_file(data, mimetype='image/png')
 
 @app.route("/game_renderer", methods=['GET', 'POST'])
 def game_renderer():
-    if not session.get('stage'):
+    return render_template("game_renderer.html")
+        
+@app.route("/login_screen/<warning_type>", methods=['GET', 'POST'])
+def login_screen(warning_type):
+    if (warning_type=='initial_ok'):
         return render_template('login.html')
     else:
-        if (not(session['stage']==2)):
-            return render_template('login.html')
-        else:
-            session['stage']=1
-            return render_template("game_renderer.html")
-
+        return render_template('login.html')
+    
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('frame_set.html')
